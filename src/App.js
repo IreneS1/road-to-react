c10e457ba66c11305bfd29d02bdacc3a34956eb9
Irene Sanchez <irenesanchez@Irenes-MacBook-Pro.local>
2021-12-14 18:49:10 -0700

    lesson 2

--- src/App.js
+++ src/App.js
@@ -19,33 +19,42 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+    <Search />
 
-      <hr />
+    <hr />
 
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+    <List />
+  </div>
+);
 
-    </div>
-  );
-}
+
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+
+const List = () => (
+  <ul>
+    {list.map((item) => {
+      return (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      );
+    })}
+  </ul>
+);
+
 
 export default App;
